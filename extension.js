acefa8a3e8dc0b0ae721a95ddb491afc5545bd13
Youssef <yussef0212@gmail.com>
2024-10-26 19:34:17 +0300

    Added Python Support: Top of file imports

--- extension.js
+++ extension.js
@@ -21,21 +21,38 @@ function activate(context) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Get Ready to have your imports sorted!');
+		// vscode.window.showInformationMessage('Get Ready to have your imports sorted!');
 		const editor = vscode.window.activeTextEditor
 		const document = editor.document
+		const language = document.languageId
 		const text = document.getText()
 		const lines = text.split("\n")
-		var sorted_lines = lines.sort((a,b)=>{
-			return a.length - b.length
-		})
+		
+		if (language === "python"){
+			var lastImport = 0
+			console.log("language is python")
+			for(var i = 0; i<lines.length;i++){
+				if (lines[i].slice(0,4) === "from" || lines[i].slice(0,6) === "import"){
+					lastImport++
+				}
+				else{
+					break
+				}
+			}
+			var to_be_sorted = lines.slice(0,lastImport)
+			var sorted_lines = to_be_sorted.sort((a,b)=>{
+				return a.length - b.length
+			})
+			var remaining_lines = lines.slice(lastImport,lines.length)
+			var final_lines = sorted_lines.concat(remaining_lines)
+			editor.edit(editBuilder =>{
+				editBuilder.replace(
+					new vscode.Range(0,0,document.lineCount,0),
+					final_lines.join("\n")
+				)
+			})
+		}
 
-		editor.edit(editBuilder =>{
-			editBuilder.replace(
-				new vscode.Range(0,0,document.lineCount,0),
-				sorted_lines.join("\n")
-			)
-		})
 
 	});
 
